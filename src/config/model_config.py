d5e438907bc6607c022609858062df3a5e9bd21a
Caesar Wong <caesar.wongw@gmail.com>
2022-12-28 09:08:44 -0800

    add model random state

--- src/config/model_config.py
+++ src/config/model_config.py
@@ -12,14 +12,13 @@ Usage:
 from yacs.config import CfgNode as CN
 from scipy.stats import lognorm, loguniform, randint
 from sklearn.metrics import f1_score, make_scorer, recall_score
+import numpy as np
 
 _C = CN()
 
 _C.MODEL = CN()
 # Model's random state
 _C.MODEL.RANDOM_STATE = 123
-# Model's strategy (dummy)
-_C.MODEL.STRATEGY = "stratified"
 # Model's max iteration
 _C.MODEL.MAX_ITER = 1000
 # Model's class weight
@@ -39,26 +38,35 @@ _C.LINEAR_MODEL.PARAM_GRID = [{"logisticregression__C": loguniform(1e-3, 1e3)}]
 
 # Tree-based model hyperparameter (param_grid)
 _C.TREE_MODEL = CN()
+# random state for tree based model
+_C.TREE_MODEL.RANDOM_STATE=[np.random.RandomState(_C.MODEL.RANDOM_STATE)]
 # list of params_grid
 _C.TREE_MODEL.PARAM_GRID = [
-{
-    "randomforestclassifier__n_estimators": randint(low=10, high=100),
-    "randomforestclassifier__max_depth": randint(low=2, high=20),
-},
-{
-    "xgbclassifier__n_estimators": randint(10, 100),
-    "xgbclassifier__max_depth": randint(low=2, high=20),
-    "xgbclassifier__learning_rate": [0.01, 0.1],
-    "xgbclassifier__subsample": [0.5, 0.75, 1],
-},
-{
-    "lgbmclassifier__n_estimators": randint(10, 100),
-    "lgbmclassifier__max_depth": randint(low=2, high=20),
-    "lgbmclassifier__learning_rate": [0.01, 0.1],
-    "lgbmclassifier__subsample": [0.5, 0.75, 1],
-}
-]
+                            {
+                                "randomforestclassifier__n_estimators": randint(low=10, high=100),
+                                "randomforestclassifier__max_depth": randint(low=2, high=20),
+                            },
+                            {
+                                "xgbclassifier__n_estimators": randint(10, 100),
+                                "xgbclassifier__max_depth": randint(low=2, high=20),
+                                "xgbclassifier__learning_rate": [0.01, 0.1],
+                                "xgbclassifier__subsample": [0.5, 0.75, 1],
+                            },
+                            {
+                                "lgbmclassifier__n_estimators": randint(10, 100),
+                                "lgbmclassifier__max_depth": randint(low=2, high=20),
+                                "lgbmclassifier__learning_rate": [0.01, 0.1],
+                                "lgbmclassifier__subsample": [0.5, 0.75, 1],
+                            }
+                            ]
 
+# default infered well performed model
+_C.TOP_MODELS = CN()
+_C.TOP_MODELS.MODEL = ["random forest (tuned)", "xgboost (tuned)", "lgbm (tuned)"]
+
+# select model for interpretation
+_C.MODEL_INTERPRET = CN()
+_C.MODEL_INTERPRET.NAME = "lgbm (tuned)"
 
 
 def get_cfg_defaults():
